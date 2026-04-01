a23f5ee0bbf650f259fe42b3d78cae0841f49d0c
David Troy <troydavidfeliciano@gmail.com>
2026-04-01 18:27:12 +0800

    fix: remove JSON.stringify and fix todo length check

--- server/routes.js
+++ server/routes.js
@@ -14,9 +14,9 @@ router.get("/todos", async (req, res) => {
 /// POST /todos
 router.post("/todos", async (req, res) => {
   const collection = getCollection();
-  
-  const newTodo = await collection.insertOne({ todo, status: false });
+  const { todo } = req.body;
 
+  const newTodo = await collection.insertOne({ todo, status: false });
   res.status(201).json({ todo, status: false, _id: newTodo.insertedId });
 })
 
